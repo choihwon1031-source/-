37b5302d63816661d1de97412736f558b5df6e9c
choihwon1031-source <choihwon1031@gmail.com>
2026-06-02 13:36:53 +0900

    Update 1234.py

--- 1234.py
+++ 1234.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 st.set_page_config(page_title="Spin Coating Thin-Film Simulator", layout="wide")
 
 
